6333c5c0eb8d9854d9c18a42b4e1f03cfe54cf35
Eremeow138 <eremeow138@yandex.ru>
2021-07-18 15:30:36 +0800

    feat: add closures

---
+++ 06_closures.js
@@ -0,0 +1,59 @@
+// функция замыкает в себе значения вышестоящего скоупа
+
+function sayHelloTo(name) {
+  const message = 'Hello ' + name;
+  return function () {
+    console.log(message);
+  };
+}
+
+const helloToElena = sayHelloTo('Elena');
+helloToElena();
+
+const helloToIgor = sayHelloTo('Igor');
+helloToIgor();
+
+// практическое применение замыканий
+
+function createFrameworkManager() {
+  const fw = ['Angular', 'React'];
+
+  return {
+    print: function () {
+      console.log(fw.join(' '));
+    },
+    add: function (framework) {
+      fw.push(framework);
+    },
+  };
+}
+
+const manager = createFrameworkManager();
+
+console.log(manager);
+// console.log(fw); // ошибка, нету такой переменной
+manager.print();
+manager.add('Vue');
+manager.print();
+
+// пример с сет таймаутом
+
+const fib = [1, 2, 3, 5, 8, 13];
+
+for (var i = 0; i < fib.length; i++) {
+  setTimeout(function () {
+    console.log(`fib[${i}] = ${fib[i]}`);
+  }, 1500);
+}
+for (var i = 0; i < fib.length; i++) {
+  (function (j) {
+    setTimeout(function () {
+      console.log(`fib[${j}] = ${fib[j]}`);
+    }, 1500);
+  })(i);
+}
+for (let i = 0; i < fib.length; i++) {
+  setTimeout(function () {
+    console.log(`fib[${i}] = ${fib[i]}`);
+  }, 1500);
+}
